ffb78a8e0c7681f1604f440eaa958bc6de87761b
Michela1983 <michela.artizzu@gmail.com>
2024-12-08 21:34:16 -0500

    Step 6: Create a function that checks if the CSV is older than 10 minutes and moves it to "Archived folder". First test executed and creation of a archived file with data stamp.

--- Original Files/script.ps1
+++ Original Files/script.ps1
@@ -53,4 +53,42 @@ if (-not (Test-Path -Path $archiveFolder)) {
     New-Item -ItemType Directory -Path $archiveFolder
 }
 
+# Define the parameters of a function to archive older CSV files
+function archiveOldFile {
+    param (
+    [string]$csvPath,         
+    [string]$archiveFolder,
+    [int]$fileMinuteAge = 10
+    )
 
+    # Verify if a CSV file exists and if it is older than 10 minutes
+    if (Test-Path -Path $csvPath) {
+        $fileInfo = Get-Item -Path $csvPath   
+        $fileAge = (Get-Date) - $fileInfo.LastWriteTime
+        
+        # Determine if file age > 10 minutes and move it to "Archived" folder
+        if ($fileAge.TotalMinutes -gt $fileMinuteAge) {
+            $timestamp = Get-Date -Format "yyyy-MM-dd-HH-mm"
+            $archivedCsvPath = Join-Path -Path $archiveFolder -ChildPath "archivedfile_$timestamp.csv"
+            Move-Item -Path $csvPath -Destination $archivedCsvPath
+            # Display a message if CSV file is moved to "Archived" folder
+            Write-Host "CSV file moved to 'Archived' folder!"
+        } else {
+            # Display an alternative message if CSV file < 10 minutes
+            Write-Host "CSV file is less than 10 minutes old. No action was taken."
+        }
+    } elseif (Test-Path -Path (Join-Path $archiveFolder "archivedfile*")) {
+        # Display a message if file has already been archived
+        Write-Host "CSV file already archived."
+        # Display a message if no file can be found
+    } else {
+        Write-Host "File cannot be found!"
+    }
+}
+
+# Run the script
+$csvPath = "E:\Vanier MEQ-4\Web Development Environment I\Assignment 01\Original Files\data.csv"
+$archiveFolder = "E:\Vanier MEQ-4\Web Development Environment I\Assignment 01\Archived"
+
+# Call the function
+archiveOldFile -csvPath $csvPath -archiveFolder $archiveFolder
